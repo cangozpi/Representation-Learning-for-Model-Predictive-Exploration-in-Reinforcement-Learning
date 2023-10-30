972c862cc6c8d3e8973aaf701155b09ad7c4b0c9
cangozpi <cangozpinar@gmail.com>
2023-10-30 23:48:19 +0300

    torch distributed nccl backend Duplicate GPU Detected error fix

--- dist_utils.py
+++ dist_utils.py
@@ -55,21 +55,28 @@ def ddp_setup(logger, use_cuda):
         assert torch.cuda.is_available() == True, "use_cuda:True is passed but cuda is not available !"
 
     if torch.cuda.is_available() and use_cuda:
-        gpu_id = "cuda:" + LOCAL_RANK % torch.cuda.device_count()
+        gpu_id = "cuda:" + str(LOCAL_RANK % torch.cuda.device_count())
         backend = "nccl"
     else:
         gpu_id = "cpu"
         backend = "gloo"
 
-    init_process_group(backend=backend)
-    if torch.cuda.is_available():
-        torch.cuda.set_device(gpu_id)
+    if torch.cuda.is_available() and use_cuda and is_agents_group_member():
+        torch.cuda.set_device(gpu_id) # GPU should only be used by the agents_group processes and each process should have a unique cuda device (otherwise see the error at: https://github.com/pytorch/torchrec/issues/328)
+    init_process_group(backend="gloo")
 
     agents_group, env_workers_group, agents_group_global_ranks, env_workers_group_global_ranks, env_workers_group_per_node_global_ranks = create_process_groups_for_training(agents_group_backend=backend, env_workers_group_backend="gloo")
 
     logger.log_msg_to_both_console_and_file(f'Initializing process with global_rank: {GLOBAL_RANK}, local_rank: {LOCAL_RANK}, local_world_size: {LOCAL_WORLD_SIZE}, global_world_size: {GLOBAL_WORLD_SIZE}, [{gpu_id}], group: {"agents_group" if GLOBAL_RANK in agents_group_global_ranks else "env_workers_group"}, group_backend: {backend if GLOBAL_RANK in agents_group_global_ranks else "gloo"}, default_backend: {backend}')
     
     return GLOBAL_WORLD_SIZE, GLOBAL_RANK, LOCAL_WORLD_SIZE, LOCAL_RANK, gpu_id, agents_group, env_workers_group, agents_group_global_ranks, env_workers_group_global_ranks, env_workers_group_per_node_global_ranks
+
+def is_agents_group_member():
+    """
+    returns True if the process belongs to the agents_group, False otherwise.
+    """
+    GLOBAL_WORLD_SIZE, GLOBAL_RANK, LOCAL_WORLD_SIZE, LOCAL_RANK = get_dist_info()
+    return GLOBAL_RANK % LOCAL_WORLD_SIZE == 0
 
 
 def create_process_groups_for_training(agents_group_backend="nccl", env_workers_group_backend="gloo"):
